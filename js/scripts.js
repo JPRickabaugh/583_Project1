34e1c188b96f2a91fcd8ee5eb9e5dd77f39fa66f
JP Rickabaugh <jprickabaugh@gmail.com>
2019-03-09 15:55:17 -0500

    progress on section 1

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+//code below based on sample code from scrollmagic site
+$(function () { // wait for document ready
+    //init
+        var controller = new ScrollMagic.Controller({
+            globalSceneOptions: {
+                triggerHook: 'onLeave'
+            }
+        });
+    
+         //get all slides
+        var slides = document.querySelectorAll("section.panel");
+    
+         //create scene for each slide
+        for (var i=0; i<slides.length; i++) {
+            new ScrollMagic.Scene({
+                triggerElement: slides[i]
+            })
+            .setPin(slides[i])
+            .addTo(controller);
+        }
+    }); 
